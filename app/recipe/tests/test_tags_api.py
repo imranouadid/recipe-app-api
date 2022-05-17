92f20e7edd5f623d3739b6715f46234162af31b5
Imran Ouadid <imran.ouadid@gmail.com>
2022-05-17 16:15:43 +0100

    Added create tag feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -63,3 +63,22 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    # Test creating a new tag
+    def test_create_tag_successful(self):
+        payload = {"name": "new test tag"}
+        self.client.post(TAGS_URL, payload)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    # Test creating a new tag with invalid payload
+    def test_create_invalid_tag(self):
+        payload = {'name': ''}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -8,7 +8,10 @@ from core.models import Tag
 
 
 # Manage tags in the database
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
+
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
@@ -17,3 +20,7 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     # Return objects for the current authenticated user only
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    # Create a new tag
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
